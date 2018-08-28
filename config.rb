82842cf01f9bafb50fda4a4d810767869c53db84
Hsieh Kai Fu <>
2018-08-28 17:44:59 +0800

    no message

--- config.rb
+++ config.rb
@@ -26,5 +26,8 @@ line_comments = false
 # preferred_syntax = :sass 
 # and then run: 
 
+
 Encoding.default_external = "utf-8" 
+Encoding.default_external = Encoding::UTF_8
+# Encoding.default_internal = Encoding::UTF_8
 sourcemap = true 
