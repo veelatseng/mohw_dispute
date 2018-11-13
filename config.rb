6dd0f2ff451d90bae1e323c0efe2018950b4d4aa
Kai Fu Hsieh <eason@tpe.hyweb.com.tw>
2018-11-13 11:48:23 +0800

    更新config.rb

--- config.rb
+++ config.rb
@@ -29,5 +29,5 @@ line_comments = false
 
 Encoding.default_external = "utf-8" 
 Encoding.default_external = Encoding::UTF_8
-# Encoding.default_internal = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 sourcemap = true 
