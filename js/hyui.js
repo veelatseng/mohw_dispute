bf9da267f40d35c9a75403994b8dd30a31867199
Kai Fu Hsieh <eason@tpe.hyweb.com.tw>
2018-11-15 17:12:58 +0800

    修正 header 按鈕、fatfooter按鈕，解決W3C error

--- js/hyui.js
+++ js/hyui.js
@@ -247,14 +247,14 @@ $(function() {
     $('.btn-fatfooter').click(function(e) {
         $(this).parent('.container').find('nav>ul>li>ul').stop(true, true).slideToggle(function() {
             if ($(this).is(':visible')) {
-                $('.btn-fatfooter').html("收合");
-                $('.btn-fatfooter').attr('name', '收合選單');
+                $('.btn-fatfooter').html("收合/CLOSE");
+                $('.btn-fatfooter').attr('name', '收合選單/CLOSE');
             } else {
-                $('.btn-fatfooter').html("展開");
-                $('.btn-fatfooter').attr('name', '展開選單');
+                $('.btn-fatfooter').html("展開/OPEN");
+                $('.btn-fatfooter').attr('name', '展開選單/OPEN');
             }
         });
-        $(this).stop(true, true).toggleClass('close');
+        $(this).stop(true, true).toggleClass('open');
     });
     /*-----------------------------------*/
     ////////img objectfix cover////////////
